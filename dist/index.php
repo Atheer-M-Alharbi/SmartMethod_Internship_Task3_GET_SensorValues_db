4f224250db6b00c2c523ca605020855169a07b21
Atheer Alharbi <79569326+Atheer-M-Alharbi@users.noreply.github.com>
2022-08-01 23:36:39 +0300

    Add files via upload

---
+++ dist/index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> 
+    <title> Welcome</title>
+</head>
+<body> 
+
+    <input type="hidden" id="value" value="<?php echo $_GET['value']; ?>" />
+    <script src="bundle.js"></script>
+
+</body>
+</html>
