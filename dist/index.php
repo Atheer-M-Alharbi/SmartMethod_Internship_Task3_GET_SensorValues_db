22685f56f876156d9b843f677639e5dca689191e
Atheer Alharbi <79569326+Atheer-M-Alharbi@users.noreply.github.com>
2022-08-02 00:01:23 +0300

    Update index.php

--- dist/index.php
+++ dist/index.php
@@ -2,9 +2,12 @@
 <head>
     <meta charset="UTF-8"> 
     <title> Welcome</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
 </head>
 <body> 
 
+    <img id="logo_img" src="logo.png" alt="logo" />
     <input type="hidden" id="value" value="<?php echo $_GET['value']; ?>" />
     <script src="bundle.js"></script>
 
